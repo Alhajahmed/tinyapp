4ef821786bc1d6dc54878258dd01282491f028f0
Ahmed <ahmed91alhaj@gmail.com>
2023-02-19 15:56:48 -0500

    Adding Get Then Post to Receive Form Submission

--- express_server.js
+++ express_server.js
@@ -3,12 +3,19 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
