9f2f1054531961763ded57d927948f01ba06c5de
Ahmed <ahmed91alhaj@gmail.com>
2023-02-20 05:51:02 -0500

    Modifying _header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
@@ -42,19 +43,28 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
+    username: req.cookies["username"],
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
   };
   res.render("urls_show", templateVars);
 });
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
